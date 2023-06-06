ad62880f8bd748619ba592cf5c27c4c873eaee33
tatsusalad <trolypoper@gmail.com>
2023-06-06 14:04:52 +0300

    last.ffm update. very scuffed, will improve in later commit

--- server/main.js
+++ server/main.js
@@ -1,10 +1,22 @@
+require('dotenv').config({ path: '/home/salad/http-lcd/server/process.env'})
 const express = require('express')
+var LastFmNode = require('lastfm').LastFmNode;
+
+var lfm = new LastFmNode({
+  api_key: process.env.LFM_API_KEY,
+  secret: process.env.LFM_SECRET,
+});
 const app = express()
+console.log(process.env)
+
 let postBody
 var request = require('request');
- function updateClient(postData){
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+        }
+    function updateClient(postData){
             var clientServerOptions = {
-                uri: 'http://192.168.0.142',
+                uri: 'http://192.168.89.196',
                 body: JSON.stringify(postBody),
                 method: 'POST',
                 headers: {
@@ -12,12 +24,25 @@ var request = require('request');
                 }
             }
             request(clientServerOptions, function (error, response) {
-                console.log(error,);
                 return;
             });
         }
 
 
-
-        postBody = 'aaaaaaaaaaa'
-        updateClient()
+            function printTrack(username) {
+            let trackStream = lfm.stream(username)
+            trackStream.on('nowPlaying', function(track) {
+                console.log(track)
+                let aa = track.artist
+                aa = JSON.stringify(aa)
+                aa = aa.replace("#", "")
+                aa = JSON.parse(aa)
+                console.log(aa)
+                postBody = aa.text + " - " + track.name
+                updateClient()
+            })
+            trackStream.start(username)
+            
+            }
+        
+            printTrack("salad-6969")
